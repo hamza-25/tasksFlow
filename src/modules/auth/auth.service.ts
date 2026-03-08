14795cc3c513827b78a2080dfced1e669962823e
hamza-25 <me677499@gmail.com>
2026-03-08 15:45:19 +0000

    register works

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,7 +2,6 @@ import  { type Request, type Response } from 'express';
 import bcrypt from 'bcryptjs';
 import authRepository from './auth.repository.js';
 import AppError from '../../utils/AppError.js';
-import jwt from 'jsonwebtoken';
 
 export const authRegister = async (req: Request, res: Response) => {
     // need validation fullName, email, password
@@ -14,18 +13,7 @@ export const authRegister = async (req: Request, res: Response) => {
         throw new AppError('Registration failed', 400);
     }
     
-    // const refreshToken = jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '7d' });
-    
-    // await authRepository.updateRefreshToken(user.id, refreshToken);
-
-    // res.cookie('refreshToken', refreshToken, {
-    //     httpOnly: true,
-    //     secure: process.env.NODE_ENV === 'production',
-    //     sameSite: 'strict',
-    //     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    // });
-    
-    const { password: _, ...userWithoutPassword } = user;
+    const { password: _password, refreshToken: _refreshToken, ...userWithoutPassword } = user;
 
     return userWithoutPassword;
 }
