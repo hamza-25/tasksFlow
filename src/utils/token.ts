71d8469fce60a3d1c3e7959f090a7629ab80c45a
hamza-25 <me677499@gmail.com>
2026-03-08 15:27:58 +0000

    implement generateRefreshToken function

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -6,7 +6,11 @@ interface payloadData {
     role: string
 }
 
-export const generateAccessToken = (payload: payloadData) => {}
-export const generateRefreshToken = (payload: payloadData) => {}
+export const generateAccessToken = (payload: payloadData) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '15m' });
+}
+export const generateRefreshToken = (payload: payloadData) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '7d' });
+}
 export const verifyAccessToken = (accessToken: string) => {}
-export const verifyRefreshToken = (accessToken: string) => {}
+export const verifyRefreshToken = (refreshToken: string) => {}
